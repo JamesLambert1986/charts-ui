d3c124dfdede504a7f1d96be878f9ab70d3cdfb2
jameslambert1986 <jameslambert1986@gmail.com>
2022-11-06 21:31:55 +0000

    lots of changes

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,7 +3,7 @@ import chart from './modules/chart.js';
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    Array.from(document.querySelectorAll('.chart__wrapper')).forEach((arrayElement) => {
+    Array.from(document.querySelectorAll('.chart')).forEach((arrayElement) => {
         chart(arrayElement);
     });
 });
--- _assets/js/modules/chart.js
+++ _assets/js/modules/chart.js
@@ -1,11 +1,9 @@
 import { ucfirst, unsnake } from './helpers.js'
 
-function chart(chartElement,min,max,type) {
+function chart(chartElement,min,max,type,guidelines) {
 
-  let chartKey = chartElement.querySelector('.chart__key');
-  let chartYaxis = chartElement.querySelector('.chart__yaxis');
-  let chartGuidelines = chartElement.querySelector('.chart__guidelines');
-
+  const chartID = `chart-${Date.now()}`;
+  let table = chartElement.querySelector('table');
 
   if(typeof min == 'undefined'){
     min = chartElement.getAttribute('data-min');
@@ -13,25 +11,68 @@ function chart(chartElement,min,max,type) {
   if(typeof max == 'undefined'){
     max = chartElement.getAttribute('data-max');
   }
-
-  // Chart key
-  if(chartKey && chartKey.childElementCount == 0){
-    createChartKey(chartElement);
+  if(typeof type == 'undefined'){
+    type = chartElement.getAttribute('data-type') ? chartElement.getAttribute('data-type') : 'bar';
   }
+  if(typeof guidelines == 'undefined' && chartElement.hasAttribute('data-guidelines')){
+    guidelines = chartElement.getAttribute('data-guidelines').split(',');
+  }
+
+
+  // Wrap the table with some divs to add functionality
+  if(!chartElement.querySelector('.table__wrapper')){
+
+    const tableWrapper = document.createElement('div');
+    tableWrapper.setAttribute('class','table__wrapper');
+
+    chartElement.append(tableWrapper);
+    tableWrapper.append(table);
+  }
+
+  if(!chartElement.querySelector('.chart__inner')){
+
+    const tableWrapper = chartElement.querySelector('.table__wrapper');
+    const chartInner = document.createElement('div');
+    chartInner.setAttribute('class','chart__inner');
+
+    chartInner.append(tableWrapper);
+    chartElement.append(chartInner);
+  }
+
+  // Create chart key if the one isn't already created
+  if(!chartElement.querySelector('.chart__key')){
+    createChartKey(chartID,chartElement);
+  }
+
+  // Create the required type input field if one isn't set
+  if(!chartElement.querySelector(':scope > [type="radio"]:checked')){
+    createChartType(chartID,chartElement,type);
+  }
+
 
   // Y Axis and Guidelines
-  if(chartYaxis && chartYaxis.childElementCount == 0){
-    createChartYaxis(chartElement);
-  }
-  if(chartGuidelines && chartGuidelines.childElementCount == 0){
-    createChartGuidelines(chartElement);
+  if(guidelines){
+    createChartYaxis(chartElement,min,max,guidelines);
+    createChartGuidelines(chartElement,min,max,guidelines);
   }
 
-  // Add css vars to cells
+  // Make sure table cells have enough data attached to them to display the chart data
   Array.from(chartElement.querySelectorAll('tbody tr')).forEach((tr, index) => {
 
     let group = tr.querySelector('td:first-child, th:first-child') ? tr.querySelector('td:first-child, th:first-child').innerHTML : '';
 
+    // Set the data numeric value if not set
+    Array.from(tr.querySelectorAll('td:not([data-numeric]):not(:first-child)')).forEach((td, index) => {
+      td.setAttribute('data-numeric',parseFloat(td.innerHTML.replace('£','')));
+    });
+
+    // Set the data label value if not set
+    Array.from(tr.querySelectorAll('td:not([data-label])')).forEach((td, index) => {
+
+      td.setAttribute('data-label',chartElement.querySelectorAll('thead th')[index].innerHTML);
+    });
+
+    // Add css vars to cells
     Array.from(tr.querySelectorAll('td[data-numeric]:not([data-numeric="0"]):not(:first-child)')).forEach((td, index) => {
 
       
@@ -70,6 +111,17 @@ function chart(chartElement,min,max,type) {
   if(chartElement.querySelector(':scope > input[value="pie"]:checked'))
     createPies(chartElement);
 
+  // Event handlers
+  const showData = chartElement.querySelectorAll(':scope > input[type="checkbox"]');
+
+  for (var i = 0; i < showData.length; i++) {
+    showData[i].addEventListener('change', function() {
+      
+      if(chartElement.querySelector(':scope > input[value="pie"]:checked'))
+        createPies(chartElement);
+    });
+  }
+
   if(chartElement.querySelector(':scope > input[type="radio"]')){
 
     const chartTypes = chartElement.querySelectorAll(':scope > input[type="radio"]');
@@ -89,40 +141,92 @@ function chart(chartElement,min,max,type) {
     }
 
   }
-
-
-
 }
 
-export const createChartKey = function(chartElement){
+export const createChartKey = function(chartID, chartElement){
 
-  let chartKey = chartElement.querySelector('.chart__key');
+  let chartKey = document.createElement("div");
+  chartKey.setAttribute('class','chart__key');
+  chartKey.setAttribute('role','presentation');
+
+  chartElement.prepend(chartKey);
 
   Array.from(chartElement.querySelectorAll('thead th')).forEach((arrayElement, index) => {
 
-    chartKey.innerHTML += `<div class="key">${arrayElement.innerText}</div>`;
+    if(index != 0){
+
+      let input = document.createElement('input');
+      input.setAttribute('name',`${chartID}-dataset-${index}`);
+      input.setAttribute('id',`${chartID}-dataset-${index}`);
+      input.setAttribute('checked',`checked`);
+      input.setAttribute('type',`checkbox`);
+
+
+      chartElement.insertBefore(input,chartKey);
+
+      let label = document.createElement('label');
+      label.setAttribute('class',`key`);
+      label.setAttribute('for',`${chartID}-dataset-${index}`);
+      label.innerHTML = `${arrayElement.innerText}`;
+
+      chartKey.append(label);
+
+    }
   });
 }
 
-export const createChartGuidelines = function(chartElement){
+export const createChartType = function(chartID,chartElement,type){
 
-  let chartGuidelines = chartElement.querySelector('.chart__guidelines');
-  const max = chartElement.getAttribute('data-max');
-  const min = chartElement.getAttribute('data-min');
+  const chartInner = chartElement.querySelector('.chart__inner');
+  const chartType = document.createElement('input');
 
-  chartGuidelines.innerHTML += `<div style="--value: 0;--percent:0%;" class="axis__point"><span>0</span></div>`;
-  chartGuidelines.innerHTML += `<div style="--value: ${max};--percent:100%;" class="axis__point"><span>${max}</span></div>`;
+  chartType.setAttribute('type','radio');
+  chartType.setAttribute('name',`${chartID}-type`);
+  chartType.value = type;
+  chartType.setAttribute('checked','checked');
+
+  chartElement.insertBefore(chartType, chartInner);
 }
 
-export const createChartYaxis = function(chartElement){
 
-  let chartYaxis = chartElement.querySelector('.chart__yaxis');
-  const max = chartElement.getAttribute('data-max');
-  const min = chartElement.getAttribute('data-min');
+export const createChartYaxis = function(chartElement,min,max,guidelines){
 
-  chartYaxis.innerHTML += `<div style="--value: 0;--percent:0%;" class="axis__point"><span>0</span></div>`;
-  chartYaxis.innerHTML += `<div style="--value: ${max};--percent:100%;" class="axis__point"><span>${max}</span></div>`;
+  const chartInner = chartElement.querySelector('.chart__inner');
+
+  const chartYaxis = document.createElement('div');
+  chartYaxis.setAttribute('class','chart__yaxis');
+
+  for (var i = 0; i < guidelines.length; i++) {
+
+    const value = parseFloat(guidelines[i].replace('£',''));
+    const percent = (100 * value) / max;
+
+    chartYaxis.innerHTML += `<div class="axis__point" style="--percent:${percent}%;"><span>${guidelines[i]}</span></div>`;
+    
+  }
+
+  chartInner.prepend(chartYaxis);
 }
+
+export const createChartGuidelines = function(chartElement,min,max,guidelines){
+
+  const tableWrapper = chartElement.querySelector('.table__wrapper');
+
+  const chartGuidelines = document.createElement('div');
+  chartGuidelines.setAttribute('class','chart__guidelines');
+
+  for (var i = 0; i < guidelines.length; i++) {
+
+    const value = parseFloat(guidelines[i].replace('£',''));
+    const percent = (100 * value) / max;
+
+    chartGuidelines.innerHTML += `<div class="guideline" style="--percent:${percent}%;"></div>`;
+  }
+
+  tableWrapper.prepend(chartGuidelines);
+
+}
+
 
 function getCoordinatesForPercent(percent) {
   const x = Math.cos(2 * Math.PI * percent);
@@ -133,23 +237,37 @@ function getCoordinatesForPercent(percent) {
 export const createPies = function(chartElement){
 
   let returnString = '';
+
+
+  let chartInner = chartElement.querySelector('.chart__inner');
+
   let pieWrapper = chartElement.querySelector('.pies');
+
+  if(!pieWrapper){
+
+    pieWrapper = document.createElement("div");
+    pieWrapper.setAttribute('class','pies');
+
+    chartInner.append(pieWrapper);
+
+  }
+
 
   Array.from(chartElement.querySelectorAll('tbody tr')).forEach((item, index) => {
 
     let paths = '';
     let tooltips = '';
-
     let cumulativePercent = 0;
-
     let total = 0;
-
     let titleKey = item.querySelectorAll('td')[0]
     let title = titleKey.innerHTML;
 
+    // Work out the total amount
     Array.from(item.querySelectorAll('td')).forEach((cell, subindex) => {
 
-      if(subindex != 0){
+      const display = getComputedStyle(cell).display;
+
+      if(subindex != 0 && display != 'none'){
 
         let value = cell.getAttribute('data-numeric');
 
@@ -161,9 +279,12 @@ export const createPies = function(chartElement){
       }
     });
 
+    // Create the paths
     Array.from(item.querySelectorAll('td')).forEach((cell, subindex) => {
 
-      if(subindex != 0){
+      const display = getComputedStyle(cell).display;
+
+      if(subindex != 0 && display != 'none'){
 
         let value = cell.getAttribute('data-numeric');
 
@@ -173,23 +294,22 @@ export const createPies = function(chartElement){
 
         let percent = value/total;
 
-        //lines[subindex-1] += `${command} ${spacer * index} ${100-percent} `;
         const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
 
-          // each slice starts where the last slice ended, so keep a cumulative percent
-          cumulativePercent += percent;
+        // each slice starts where the last slice ended, so keep a cumulative percent
+        cumulativePercent += percent;
 
-          const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
+        const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
 
-          // if the slice is more than 50%, take the large arc (the long way around)
-          const largeArcFlag = percent > .5 ? 1 : 0;
+        // if the slice is more than 50%, take the large arc (the long way around)
+        const largeArcFlag = percent > .5 ? 1 : 0;
 
-          // create an array and join it just for code readability
-          const pathData = [
-            `M ${startX} ${startY}`, // Move
-            `A 100 100 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
-            `L 0 0`, // Line
-          ].join(' ');
+        // create an array and join it just for code readability
+        const pathData = [
+          `M ${startX} ${startY}`, // Move
+          `A 100 100 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Arc
+          `L 0 0`, // Line
+        ].join(' ');
 
         paths += `<path d="${pathData}"></path>`;
         tooltips += `<foreignObject x="-70" y="-70" width="140" height="140" style="transform: rotate(90deg)"><div><span class="h5 mb-0"><span class="total d-block">${ucfirst(unsnake(title))}</span> ${ucfirst(unsnake(cell.getAttribute('data-label')))}<br/> ${cell.innerHTML}</span></div></foreignObject>`;
@@ -205,10 +325,21 @@ export const createPies = function(chartElement){
 export const createLines = function(chartElement,min,max){
 
 
-  console.log('hi')
-
   let returnString = '';
+
+  let tableWrapper = chartElement.querySelector('.table__wrapper');
+
   let linesWrapper = chartElement.querySelector('.lines');
+
+  if(!linesWrapper){
+
+    linesWrapper = document.createElement("div");
+    linesWrapper.setAttribute('class','lines');
+
+    tableWrapper.prepend(linesWrapper);
+
+  }
+
 
   let items = Array.from(chartElement.querySelectorAll('tbody tr'));
 
